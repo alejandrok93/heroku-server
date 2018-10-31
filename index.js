e655f319379efad1b26336b8eabf4699464a75aa
alejandrok93 <alejandrok93@gmail.com>
2018-10-31 10:00:43 -0700

    changed port to break the server

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
-const port = process.env.PORT || 8000;
+const port = 8000;
 const instance = server.listen(port, () =>
-	console.log(`\n=== Server running on port: ${instance.address().port} ===\n`)
+  console.log(`\n=== Server running on port: ${instance.address().port} ===\n`)
 );
